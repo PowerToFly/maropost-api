bc395e4e30ed1a03ed51f68a89939091d95d7a36
the-maniac <yura.pilguk@gmail.com>
2019-01-03 14:08:39 +0200

    added support for brand do not message me lists

---
+++ maropost_api/brands.py
@@ -0,0 +1,46 @@
+from base import MaropostBase
+
+
+class MaropostBrandDNM(MaropostBase):
+
+    def all_info(self):
+        page = 1
+        total_pages = 2
+        while page < total_pages:
+            result = self.get_contacts_from_page(page)
+            if result:
+                total_pages = result[0].get('total_pages') + 1
+            page += 1
+            yield result
+
+    def get_info_from_page(self, page=1):
+        response = self.browser.get('/brand_unsubscribes.json', params={'page': page})
+        return self.validator(response).validate()
+
+    def unsubscribe_contact(self, data, **kwargs):
+        """
+        can be used uid or email and brand_name or brand_id
+        example:
+        {
+          "brand_unsubscribe": {
+            "uid": "abcd",
+            "email": "demo@email.com",
+            "brand_name": "brand_1",
+            "brand_id": "brand_id"
+          }
+        }
+        """
+        response = self.browser.post('/brand_unsubscribes.json', data=data, **kwargs)
+        return self.validator(response).validate()
+
+    def find_by_email(self, email):
+        response = self.browser.get('/brand_unsubscribes/email.json?email={}'.format(email.replace('+', '%2B')))
+        return self.validator(response).validate()
+
+    def find_by_email_in_brand(self, email, brand_name):
+        response = self.browser.get('/brand_unsubscribes/email.json?email={}&brand={}'.format(email.replace('+', '%2B'), brand_name))
+        return self.validator(response).validate()
+
+    def delete_by_email_from_brand(self, email, brand_name):
+        response = self.browser.delete('/brand_unsubscribes/delete.json?email={}&brand={}'.format(email.replace('+', '%2B'), brand_name))
+        return self.validator(response).validate()
